cc3a79f6d34d6509ba81a5d753ef23d37453e6b8
mansoor <manzooravail@gmail.com>
2025-02-02 19:34:21 +0530

    Update register.tsx

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { useAuth } from "@/context/AuthContext";
 import { useRouter } from "next/router";
 import toast from "react-hot-toast";
